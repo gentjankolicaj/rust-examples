b3f1ca4ca4818fb9c6feddfa215f8288394b889a
gentjankolicaj <>
2025-04-05 12:16:34 +0200

    day 11.

---
+++ src/types/casting_0.rs
@@ -0,0 +1,18 @@
+// Suppress all warnings from casts which overflow.
+#![allow(overflowing_literals)]
+
+fn main() {
+    let decimal = 65.4321_f32;
+
+    // Error! No implicit conversion
+  //  let integer: u8 = decimal;
+
+    // Explicit conversion
+    let integer = decimal as u8;
+    let character = integer as char;
+
+
+    println!("Casting: {} -> {} -> {}", decimal, integer, character);
+
+
+}
---
+++ src/types/casting_2.rs
@@ -0,0 +1,21 @@
+// Suppress all warnings from casts which overflow.
+#![allow(overflowing_literals)]
+
+fn main() {
+    // Since Rust 1.45, the `as` keyword performs a *saturating cast*
+    // when casting from float to int. If the floating point value exceeds
+    // the upper bound or is less than the lower bound, the returned value
+    // will be equal to the bound crossed.
+
+    // This behavior incurs a small runtime cost and can be avoided
+    // with unsafe methods, however the results might overflow and
+    // return **unsound values**. Use these methods wisely:
+    unsafe {
+        // 300.0 as u8 is 44
+        println!(" 300.0 as u8 is : {}", 300.0_f32.to_int_unchecked::<u8>());
+        // -100.0 as u8 is 156
+        println!("-100.0 as u8 is : {}", (-100.0_f32).to_int_unchecked::<u8>());
+        // nan as u8 is 0
+        println!("   nan as u8 is : {}", f32::NAN.to_int_unchecked::<u8>());
+    }
+}
