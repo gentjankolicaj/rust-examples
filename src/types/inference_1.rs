041b2d3777547ca3496b74f7bb15f4d29a8774c8
gentjankolicaj <>
2025-04-07 11:41:47 +0200

    day 12.

--- src/types/inference_1.rs
+++ src/types/inference_1.rs
@@ -4,18 +4,25 @@
 //2.It does more than looking at the type of the value expression during an initialization.
 //3.It also looks at how the variable is used afterwards to infer its type.
 
+//Derive debug trait, for print
+#[derive(Debug)]
+enum Switch{
+    ON,
+    OFF
+}
+
 fn main() {
     //In below example compiler does 'type inference' of vec based on parameter type on function.
-    let a = 10i8; //a is identifier,10 is literal,i8 is suffixed type, let a=10i8; is variable binding.
+    let light_switch = Switch::ON;
 
     //Declare another variable 'v'
     //v is a empty growable array and it will hold values
     let mut v = Vec::new();
     //at this point compiler doesn't know that type of 'v',it just knows that it's a vector of something
 
-    //By calling method push and passing variable binding a
-    //Compiler infers that type of 'v' is i8.
-    v.push(a);
+    //By calling function push and passing variable binding light_switch
+    //Compiler infers that type of 'v' is Switch.
+    v.push(light_switch);
 
     println!("{:?}", v);
 }
--- src/types/inference_2.rs
+++ src/types/inference_2.rs
@@ -4,18 +4,25 @@
 //2.It does more than looking at the type of the value expression during an initialization.
 //3.It also looks at how the variable is used afterwards to infer its type.
 
+//Derive debug trait, for print
+#[derive(Debug)]
+struct User{
+    id: i64,
+    time_stamp: i64,
+}
+
 fn main() {
     //In below example compiler does 'type inference' of vec based on parameter type on function.
-    let a = 10i8; //a is identifier,10 is literal,i8 is suffixed type, let a=10i8; is variable binding.
+    let john_doe =User{id:101,time_stamp:11111111};
 
     //Declare another variable 'v'
     //v is a empty growable array and it will hold values
     let mut v = Vec::new();
     //at this point compiler doesn't know that type of 'v',it just knows that it's a vector of something
 
-    //By calling method push and passing variable binding a
-    //Compiler infers that type of 'v' is i8.
-    v.push(a);
+    //By calling function push and passing variable binding john_doe
+    //Compiler infers that type of 'v' is User.
+    v.push(john_doe);
 
     println!("{:?}", v);
 }
