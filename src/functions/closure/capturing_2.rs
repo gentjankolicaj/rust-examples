f083db3be65c02dfba7cdf58d4b0baa1f3af628d
gentjankolicaj <>
2025-04-21 11:37:14 +0200

    day 22.

---
+++ src/functions/closure/capturing_2.rs
@@ -0,0 +1,30 @@
+/*
+- Closures can capture variables:
+1.by reference: &T
+2.by mutable reference: &mut T
+3.by value: T
+*/
+
+
+fn main() {
+    let mut count = 0;
+    // A closure to increment `count` could take either `&mut count` or `count`
+    // but `&mut count` is less restrictive so it takes that. Immediately
+    // borrows `count`.
+    //
+    // A `mut` is required on `inc` because a `&mut` is stored inside. Thus,
+    // calling the closure mutates `count` which requires a `mut`.
+    let mut inc = || {
+        count += 1;
+        println!("`count`: {}", count);
+    };
+
+    // Call the closure using a mutable borrow.
+    inc();
+
+    // The closure still mutably borrows `count` because it is called later.
+    // An attempt to reborrow will lead to an error.
+    // let _reborrow = &count;
+    // try uncommenting this line.
+    inc();
+}
